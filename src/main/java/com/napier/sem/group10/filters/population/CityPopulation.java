33933dd20ea680af8a3beba6f515997d107bba48
mike <07011787@live.napier.ac.uk>
2021-02-23 15:36:17 +0000

    New branch for pop_count

---
+++ src/main/java/com/napier/sem/group10/filters/population/CityPopulation.java
@@ -0,0 +1,22 @@
+package com.napier.sem.group10.filters.population;
+
+import com.napier.sem.group10.CityCommandHandler;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class CityPopulation extends CityCommandHandler {
+    @Override
+    public String getCommand() {
+        return "CityPopulation";
+    }
+
+    @Override
+    public PreparedStatement prepareStatement(Connection connection, Map<String, String> args) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("SELECT city.Name as Name, city.Population as TotalPop FROM city WHERE city.Name = ? ");
+        statement.setString(1, args.get("name"));
+        return statement;
+    }
+}
---
+++ src/main/java/com/napier/sem/group10/filters/population/CountryPop.java
@@ -0,0 +1,23 @@
+package com.napier.sem.group10.filters.population;
+
+import com.napier.sem.group10.CountryCommandHandler;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class CountryPop extends CountryCommandHandler{
+
+    @Override
+    public String getCommand() {
+        return "CountryPop";
+    }
+
+    @Override
+    public PreparedStatement prepareStatement(Connection connection, Map<String, String> args) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("SELECT country.Name as name, country.Population FROM country WHERE country.Name = ? ");
+        statement.setString(1, args.get("Name"));
+        return statement;
+    }
+}
