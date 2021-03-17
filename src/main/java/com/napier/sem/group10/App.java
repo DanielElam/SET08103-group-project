f2bb1d3f6c62811224c68d893d0747fbc7de1e71
Daniel <dan@dandev.uk>
2021-03-17 10:59:18 +0000

    Revert "Proofread Code of Conduct"

--- src/main/java/com/napier/sem/group10/App.java
+++ src/main/java/com/napier/sem/group10/App.java
@@ -16,7 +16,6 @@ import java.util.stream.Stream;
 import com.napier.sem.group10.filters.city.*;
 import com.napier.sem.group10.filters.capitalcity.*;
 import com.napier.sem.group10.filters.country.*;
-import com.napier.sem.group10.filters.language.Language;
 import com.napier.sem.group10.filters.population.*;
 import fi.iki.elonen.NanoHTTPD;
 
@@ -89,8 +88,6 @@ public class App extends NanoHTTPD {
         registerHandler(new CountryPop());
         registerHandler(new DistrictPop());
         registerHandler(new CityPopulation());
-
-        registerHandler(new Language());
 
     }
 
@@ -173,7 +170,7 @@ public class App extends NanoHTTPD {
             ICommandHandler handler = _resultHandlers.getOrDefault(command, null);
 
             if (handler == null) {
-                System.out.printf("No handler registered for command \"%s\"%n", command);
+                System.out.println(String.format("No handler registered for command \"%s\"", command));
                 return newFixedLengthResponse("!ERROR");
             }
 
