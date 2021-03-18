5f0f69b3a322b66449f7d351c2904ef27474a13c
mike <07011787@live.napier.ac.uk>
2021-03-18 15:47:53 +0000

    Test for testing in Intellij

--- src/tests/java/test1.java
+++ src/tests/java/test1.java
@@ -1,9 +1,27 @@
+
+import fi.iki.elonen.NanoHTTPD;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 
 class test1
 {
+
+    //static ICommandHandler IChand;
+
+
     @Test
-    void unitTest()
-    {}
+     void name()
+    {
+
+    }
+
+    @Test
+    void testTest()
+    {
+        assertEquals(5,5);
+    }
+
 }
